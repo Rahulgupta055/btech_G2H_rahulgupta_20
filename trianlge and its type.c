1f3314de08f4125193a5ceb0dee9dfa095d15bd3
Rahulgupta055 <dreamer07rahul@gmail.com>
2026-02-20 12:01:03 +0530

    Add files via upload

---
+++ trianlge and its type.c
@@ -0,0 +1,26 @@
+//to check if a triangle is valid or not and determine its type.
+
+#include <stdio.h>
+int main()
+{float s1,s2,s3;
+  printf("enter 3 sides:");
+  scanf("%f%f%f",&s1 ,&s2, &s3);
+  if (s1+s2>s3){
+    printf("Valid triangle \n");
+     if (s1==s2 &&  s2==s3 &&  s3==s1){
+       printf("Equilateral triangle ");
+     }
+     else if (s1==s2 || s2==s3 || s3==s1) {
+       printf("isosceles triangle");
+     }
+     else{
+       printf("scalene traingle");
+     }
+ }
+  else {
+    printf("not valid triangle");
+   }
+  return 0;
+}
+
+ 
